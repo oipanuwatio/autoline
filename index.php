26c28c4699256a08415a51ad83547ace9573e982
oipanuwatio <oipanuwatio@gmail.com>
2017-11-29 21:28:12 +0700

    Initial...

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 require_once('./vendor/autoload.php');
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient; use \LINE\LINEBot;
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
-$channel_token = '1v2OUa9tuMIiDhEg57ANbsRaBDbBGP9nlCC+Dpvt5HrsQ+LqcrImWPUBkH8re/pwqxv56d15kZeMoU/vQ0zuzPFlbhFM7AhRMZwLr SkLdcjbFurwXGOyHLt8MdgzLfAe7r0BsQV5cATlUanW3OgJewdB04t89/1O/w1cDnyilFU=';
-$channel_secret = '9b2c7349ea939ef723a3cb453d774c86';
+$channel_token = 'H01UznWoQ0ZQcnniY4SBGn+P9vXMido6CFJM8EJsYoj42OUe9Ge52sXfOqX0kk4n3/gR+kQw7LgU63yaDvfe/9ZXErHJu+X4dWUld2Mz8vRl9Fu+Ddf2kthYB9hMhCkO+Nct3yXXbeLCt+HY69Zg/wdB04t89/1O/w1cDnyilFU=';
+$channel_secret = 'ecb5892b7c1b8d11cee410475af0a310';
 //Get message from Line API
 $content = file_get_contents('php://input'); $events=json_decode($content, true);
 if (!is_null($events['events'])) { //Loop through each event
